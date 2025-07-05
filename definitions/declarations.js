5838e1d912ca7d233395a1aa2646a030e931eee3
BRK YLMZ <brkylmz999@gmail.com>
2025-07-05 20:25:46 +0000

    sources

--- definitions/declarations.js
+++ definitions/declarations.js
@@ -1,12 +1,12 @@
 // Add the tables you will use in repo to avoid hardcoding and get the dependency graph and documentation benefits
 
 const tables = [
-  { name: "distribution_centers", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
-  ,{ name: "events", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
-  ,{ name: "order_items", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
-  ,{ name: "orders", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
-  ,{ name: "users", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
-  ,{ name: "products", database: "the_look_ecommerce_eu",schema: "fit-crow-461418-s0" }
+  { name: "distribution_centers", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
+  ,{ name: "events", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
+  ,{ name: "order_items", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
+  ,{ name: "orders", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
+  ,{ name: "users", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
+  ,{ name: "products", database: "fit-crow-461418-s0",schema: "the_look_ecommerce_eu" }
 
 
 ];
